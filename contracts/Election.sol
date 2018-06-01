0edaf5c723711937f39e5a4a08d2a7f25f434f9a
papajo <joshipv2@gmail.com>
2018-06-01 17:18:28 -0400

    Smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.2;
+
+contract Election {
+    //Store candidate
+    //Read candidate
+    //Constructor
+    string public candidate;
+    function Election () public {
+        candidate = "Candidate 1";
+    }
+}
